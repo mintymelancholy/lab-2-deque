207ecbd3b53bd72acc8e8c8dd88636f1e089b664
ethyn minter <120231772+mintymelancholy@users.noreply.github.com>
2026-04-06 15:41:38 -0700

    added start

---
+++ Deque.py
@@ -0,0 +1,33 @@
+import array
+
+
+class ArrayInt:
+  # constructor, set up the array
+  def __init__(self, size=20):
+    if size < 1:
+      size = 20 #copied & edited from last lab
+    self.size = size 
+    self.lastIndex = 0
+    self.theArray = array.array('i', [0] * size)  
+
+
+
+  def addTail(self):
+
+    return self
+
+
+  def addTail(self):
+    return self
+  
+  
+  def addTail(self):
+    return self
+  
+
+  def addTail(self):
+    return self
+  
+
+  def addTail(self):
+    return self
